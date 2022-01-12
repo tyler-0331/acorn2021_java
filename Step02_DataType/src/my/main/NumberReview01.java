ff7050d96320957b222328b8b5e909425260284a
Tyler Choi <choi_kyu0331@naver.com>
2022-01-12 12:04:17 +0900

    Number type practice 추가함

---
+++ Step02_DataType/src/my/main/NumberReview01.java
@@ -0,0 +1,14 @@
+package my.main;
+
+public class NumberReview01 {
+	public static void main(String[] args) {
+		byte num1 = 20;
+		int num2 = 30;
+		
+		int a=num2;
+		int b=(int)num1;
+		
+		System.out.println(b);
+		
+	}
+}
